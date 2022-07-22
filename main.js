3885dbd815c859780066fceb21e2cba90f1f1866
devindersinghnagi <98323355+devindersinghnagi@users.noreply.github.com>
2022-07-22 05:57:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_url+'"/>';
+    }  )  }   ;
+    
+    console.log('m15 version:',m15.version);
+
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/OFdLmyrkv/model.json',modelLoaded);
+    //put teachabel link
+    function modelLoaded() {
+        console.log('Model Loaded!');
+    }
+
+    function check()
+    {
+        img = document.getElementById('captured_image');
+        classifier.classify(img, gotResult);
+    }
+
+    function gotResult(error, results) {
+        if (error) {
+            console.error(error);
+        }else {
+            console.log(results);
+            document.getElementById("result_object_name").innerHTML = results[0].label
+            document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
